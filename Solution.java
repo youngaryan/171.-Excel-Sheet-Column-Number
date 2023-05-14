d069136ba178b31f8fdeb94bef8f95e767d98fc2
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-05-14 11:33:04 +0100

    finally done passes all the tests

--- Solution.java
+++ Solution.java
@@ -4,26 +4,23 @@ import java.util.Map;
 public class Solution {
     public int titleToNumber(String columnTitle) {
 
-        String numStr = "";
-        char[] chars = columnTitle.toCharArray();
-        Map<Character, String> alphabetMap = new HashMap<>();
+        int num = 0 ;
+        Map<Character, Integer> alphabetMap = new HashMap<>();
         for (int i = 0; i < 26; i++) {
             char alphabet = (char) ('A' + i);
-            alphabetMap.put(alphabet, String.valueOf(i+1));
+            alphabetMap.put(alphabet, i+1);
         }
 
-        if (columnTitle.length()<2){
-            for (char aChar : chars) {
-               if (alphabetMap.containsKey(aChar)) numStr += alphabetMap.get(aChar);
-          }
 
-            return Integer.parseInt(numStr);
-        }else{
-            int length = (columnTitle.length()-1)*26;
-            numStr += alphabetMap.get(columnTitle.charAt(columnTitle.length()-1));
-            //throw new RuntimeException(numStr);
-            return Integer.parseInt(numStr)+length;
+        for (int i = 0; i < columnTitle.length()-1; i++) {
+            num += alphabetMap.get(columnTitle.charAt(i))*(int)Math.pow(26,columnTitle.length()-1-i);
+
         }
+
+        num+=alphabetMap.get(columnTitle.charAt(columnTitle.length()-1));
+        //throw new RuntimeException(num+String.valueOf(alphabetMap));
+        return num;
+
 
 
 
--- main.java
+++ main.java
@@ -1,6 +1,8 @@
 public class main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.titleToNumber("ZY"));
+        System.out.println(s.titleToNumber("FXSHRXW"));
+        //FXSHRXW
+        //exp: 2147483647
     }
 }
